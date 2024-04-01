bad81c520c15147153f0b14071a49f51dd65ba98
Daniel <daniel.mieleniewski@gmail.com>
2024-04-01 11:15:09 +0100

    Initial

---
+++ MainMethodAssignment/MathOpearators.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    // Defines a class to perform various mathematical operations
+    class MathOpearators
+    {
+        // Method 1: Adds two integers and returns the result
+        public static int exampleMethod(int inputOne, int inputTwo)
+        {
+            return inputOne + inputTwo;
+        }
+
+        // Method 2: Subtracts two decimals and returns the result 
+        public static decimal exampleMethod(decimal inputOne, decimal inputTwo)
+        {
+            return inputOne - inputTwo;
+        }
+
+        // Method 3: Attempts to convert two strings to integers. 
+        // If successful, multiplies them and returns the result as a string. 
+        // If not, returns an error message.
+        public static string exampleMethod(string inputOne, string inputTwo)
+        {
+            if (int.TryParse(inputOne, out int num1) && int.TryParse(inputTwo, out int num2))
+            {
+                int result = num1 * num2;
+                return result.ToString();
+            }
+            else
+            {
+                return "Invalid input: Strings could not be converted to integers";
+            }
+        }
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integer Calculation
+            int intResult = MathOpearators.exampleMethod(1, 2);
+            Console.WriteLine("the int result is: " + intResult);
+            Console.WriteLine("Press enter");
+            Console.ReadLine();
+
+            // Decimal Calculation
+            decimal decimalResult = MathOpearators.exampleMethod(1.33m, 1.32m);
+            Console.WriteLine("The decimal result is: " + decimalResult);
+            Console.WriteLine("Press enter");
+            Console.ReadLine();
+
+            // String Calculation
+            string stringDecimalResult = MathOpearators.exampleMethod("4", "8");
+            Console.WriteLine("The string result in: " + stringDecimalResult);
+            Console.ReadLine();
+        }
+    }
+
+}
